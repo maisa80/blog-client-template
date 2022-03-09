53e54528797f54de35be3e37587ff4ec00e1370e
maisa abusalem <heart.webd@gmail.com>
2022-03-09 10:47:07 +0100

    change list to table

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     fetchAllPosts();
 }
-
+const tableBody = document.querySelector('#table tbody');
 async function fetchAllPosts() {
     try {
         const response = await fetch('http://localhost:5000/posts')
@@ -10,18 +10,21 @@ async function fetchAllPosts() {
 
         let html = ''
         for (let post of posts) {
-            html += `
-                <td class="list-group-item">
-                    <p>${post.title} <br> </p>
-                    <p><span class="date">- ${post.date}</span> <br></p>
-                    <p>${post.content}<br></p>
-                    <p><span class="autor">Author: ${post.author}</span></p>
-                    <div>
-                        <a href="update-post.html?id=${post._id}">Update</a> |
-                        <a href="#" class="delete-post-link" data-post-id="${post._id}">Delete</a> 
-                    </div>
+            
+            tableBody.innerHTML += `
+            <tr>
+                <td>${post.title}</td>
+                <td>${post.author}</td>
+                <td>${post.date}</td>
+                <td>
+                    <ul>
+                        ${post.content}
+                    </ul>
                 </td>
-            `
+                <td><button class="btn btn-dark"><a href="update-post.html?id=${post._id}">Update</a></button>
+                <button class="btn btn-dark"><a href="#" class="delete-post-link" data-post-id="${post._id}">Delete</a></button></td>
+            </tr>
+        `;
         }
 
         document.getElementById('post-list').innerHTML = html;
@@ -29,3 +32,15 @@ async function fetchAllPosts() {
         console.log(error)
     }
 }
+// html += `
+//                 <td class="list-group-item">
+//                     <p>${post.title} <br> </p>
+//                     <p><span class="date">- ${post.date}</span> <br></p>
+//                     <p>${post.content}<br></p>
+//                     <p><span class="autor">Author: ${post.author}</span></p>
+//                     <div>
+//                         <a href="update-post.html?id=${post._id}">Update</a> |
+//                         <a href="#" class="delete-post-link" data-post-id="${post._id}">Delete</a> 
+//                     </div>
+//                 </td>
+//             `
