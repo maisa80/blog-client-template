97903eef4b721ce167195b8a44f352a2b60ac4af
isakbet <yosephbet@gmail.com>
2022-03-11 16:50:14 +0100

    select the form & add an eventlistener on the form

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -1,22 +1,25 @@
 window.onload = function () {
   fetchAllPosts();
-}
+};
 
 async function fetchAllPosts() {
-
   const urlParams = new URLSearchParams(window.location.search);
   const postId = urlParams.get("id");
 
   try {
-
     const response = await fetch(`http://localhost:5000/posts/${postId}`);
     const data = await response.json();
 
     document.getElementById("title").value = data.title;
     document.getElementById("author").value = data.author;
     document.getElementById("content").value = data.content;
-
   } catch (error) {
     throw new Error(error);
   }
+}
+
+async function submitUpdates(postId) {
+  let updateForm = document.getElementById("form-update-post");
+  updateForm.addEventListener("submit", async function () {    
+  });
 }
