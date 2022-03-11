1d3768816190e3af3bae60d8adc1137741d0c9dd
isakbet <yosephbet@gmail.com>
2022-03-11 16:27:31 +0100

    use the fetched post data to prefill the form

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -12,6 +12,10 @@ async function fetchAllPosts(urlParams) {
   try {
     const response = await fetch(`http://localhost:5000/posts/${postId}`);
     const data = await response.json();
+
+    document.getElementById("title").value = data.title;
+    document.getElementById("author").value = data.author;
+    document.getElementById("content").value = data.content;
   } catch (error) {
     console.log(error);
   }
