b93702ba03c2c5f2f098fb9f2a5ad2d688b0fe17
isakbet <yosephbet@gmail.com>
2022-03-11 16:05:09 +0100

    use urlParams to retrieve the postId

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -2,6 +2,9 @@ window.onload = function () {
   console.log(window.location.search);  
   const queryString = window.location.search;  
   const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams.get('id'))
+
+  fetchAllPosts(urlParams);
 
   
 };
