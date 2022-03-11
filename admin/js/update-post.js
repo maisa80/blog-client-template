4839b664e723cd6c700cae05cfed5becb17a7dee
isakbet <yosephbet@gmail.com>
2022-03-11 16:43:30 +0100

    refactor the fetchAllPosts function

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -1,22 +1,22 @@
 window.onload = function () {
-  console.log(window.location.search);
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  console.log(urlParams.get("id"));
+  fetchAllPosts();
+}
 
-  fetchAllPosts(urlParams);
-};
+async function fetchAllPosts() {
 
-async function fetchAllPosts(urlParams) {
-  let postId = urlParams.get("id");
+  const urlParams = new URLSearchParams(window.location.search);
+  const postId = urlParams.get("id");
+
   try {
+
     const response = await fetch(`http://localhost:5000/posts/${postId}`);
     const data = await response.json();
 
     document.getElementById("title").value = data.title;
     document.getElementById("author").value = data.author;
     document.getElementById("content").value = data.content;
+
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 }
