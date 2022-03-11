a8d75a22b39c079e041ba815a10ee3be2fe2c9d4
isakbet <yosephbet@gmail.com>
2022-03-11 14:02:04 +0100

    add 'URLSearchParams' to extract the queryString

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -1,4 +1,7 @@
 window.onload = function () {
   console.log(window.location.search);  
   const queryString = window.location.search;  
+  const urlParams = new URLSearchParams(queryString);
+
+  
 };
